505a12e82436c95d1a19096e3594854ac744427c
Aswin Hariram <aswincseskct@gmail.com>
2026-01-04 12:28:10 +0530

    Time: 499 ms (57.36%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,25 +1,16 @@
-# Write your MySQL query statement below
+
 select 
-    book_id,
-    title,
-    author,
-    genre,
-    publication_year,
-    current_borrowers
-from (
-    select 
-        l.book_id,
-        l.title,
-        l.author,
-        l.genre,
-        l.publication_year,
-        sum(coalesce(return_date,1)) as current_borrowers,
-        l.total_copies
-    from library_books l
-    join borrowing_records b
-    on l.book_id = b.book_id
-    where b.return_date is null 
-    group by book_id
-    having sum(coalesce(return_date,1)) = l.total_copies
-)as t
+    l.book_id,
+    l.title,
+    l.author,
+    l.genre,
+    l.publication_year,
+    COUNT(b.book_id) AS current_borrowers
+    
+from library_books l
+join borrowing_records b
+on l.book_id = b.book_id
+where b.return_date is null 
+group by l.book_id,l.total_copies
+HAVING COUNT(b.book_id) = l.total_copies
 ORDER BY current_borrowers DESC, title ASC;
