52774d51da55ee1c04588d20162260e839b3c225
Aswin Hariram <aswincseskct@gmail.com>
2026-02-03 23:34:29 +0530

    Time: 324 ms (23.22%) | Memory: 0B (100.00%) - LeetSync

--- 4091-find-golden-hour-customers/find-golden-hour-customers.sql
+++ 4091-find-golden-hour-customers/find-golden-hour-customers.sql
@@ -1,36 +1,30 @@
-# Write your MySQL query statement below
-
-with new_table as (
-select
-customer_id,
-time(order_timestamp) as order_time,
-order_amount,
-payment_method,
-order_rating
-from restaurant_orders
-),
-
-master_table as (
-select
-customer_id,
-count(customer_id) as total_orders,
-sum(case when order_time >= '11:00:00' and order_time <= '14:00:00' then 1 else 0 end) as afternoon_hour,
-sum(case when order_time >= '18:00:00' and order_time <= '21:00:00' then 1 else 0 end) as evening_hour,
-round(avg(order_rating), 2) as avg_rating,
-count(order_rating) as total_rating,
-count(order_rating) / count(customer_id) * 100 as rating_percentage
-from new_table
-group by customer_id
-)
-
-select
-customer_id,
-total_orders,
-round(((afternoon_hour + evening_hour) / total_orders) * 100) as peak_hour_percentage,
-avg_rating as average_rating
-from master_table
-where rating_percentage >= 50
-having total_orders >= 3
-and peak_hour_percentage >= 60
-and average_rating >= 4.0
-order by average_rating desc, customer_id desc;
+SELECT 
+    customer_id,
+    COUNT(*) AS total_orders,
+    ROUND(
+        SUM(
+            CASE
+                WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
+                  OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
+                THEN 1
+                ELSE 0
+            END
+        ) * 100.0 / COUNT(*)
+    ) AS peak_hour_percentage,
+    ROUND(AVG(order_rating), 2) AS average_rating
+FROM restaurant_orders
+GROUP BY customer_id
+HAVING COUNT(*) >= 3
+   AND (
+        SUM(
+            CASE
+                WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
+                  OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
+                THEN 1
+                ELSE 0
+            END
+        ) * 100.0 / COUNT(*)
+       ) >= 60
+   AND (COUNT(order_rating) * 100.0 / COUNT(*)) >= 50
+   AND AVG(order_rating) >= 4.0
+ORDER BY average_rating DESC, customer_id DESC;
