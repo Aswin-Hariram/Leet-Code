ee4bfea374a7b156b34439360f6efc733b2e1915
Aswin Hariram <aswincseskct@gmail.com>
2026-03-16 09:46:17 +0530

    Time: 207 ms (26.89%) | Memory: 61.6 MB (47.59%) - LeetSync

---
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        set<int, greater<int>> st;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                
+                // size 0 rhombus
+                st.insert(grid[i][j]);
+                
+                for(int k = 1; ; k++){
+                    
+                    if(i + 2*k >= m || j-k < 0 || j+k >= n) break;
+                    
+                    int sum = 0;
+                    
+                    int r = i, c = j;
+                    
+                    // top -> right
+                    for(int t = 0; t < k; t++){
+                        sum += grid[r+t][c+t];
+                    }
+                    
+                    // right -> bottom
+                    for(int t = 0; t < k; t++){
+                        sum += grid[r+k+t][c+k-t];
+                    }
+                    
+                    // bottom -> left
+                    for(int t = 0; t < k; t++){
+                        sum += grid[r+2*k-t][c-t];
+                    }
+                    
+                    // left -> top
+                    for(int t = 0; t < k; t++){
+                        sum += grid[r+k-t][c-k+t];
+                    }
+                    
+                    st.insert(sum);
+                }
+            }
+        }
+        
+        vector<int> ans;
+        for(int x : st){
+            ans.push_back(x);
+            if(ans.size() == 3) break;
+        }
+        
+        return ans;
+    }
+};
