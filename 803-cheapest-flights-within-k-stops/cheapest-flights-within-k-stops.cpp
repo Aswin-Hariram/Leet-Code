bdd366f6626cb3bbaa9544a06ed63b53a5b16f76
Aswin Hariram <aswincseskct@gmail.com>
2026-05-01 22:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (73.03%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -4,27 +4,44 @@ using namespace std;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-
-        const int INF = 1e9;
-        vector<int> dist(n, INF);
-        dist[src] = 0;
-
-        // Relax edges k+1 times
-        for (int i = 0; i <= k; i++) {
-            vector<int> temp = dist;
-
-            for (auto &f : flights) {
-                int u = f[0];
-                int v = f[1];
-                int w = f[2];
-
-                if (dist[u] != INF && dist[u] + w < temp[v]) {
-                    temp[v] = dist[u] + w;
-                }
-            }
-            dist = temp;
+        // adjacency list: node -> list of (neighbor, cost)
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto &f : flights) {
+            int u = f[0], v = f[1], w = f[2];
+            adj[u].push_back({v, w});
         }
 
-        return dist[dst] == INF ? -1 : dist[dst];
+        // queue holds: (node, cost so far, stops used)
+        queue<tuple<int,int,int>> q;
+        q.push({src, 0, 0});
+
+        // visited[node] = minimum cost found so far
+        vector<int> visited(n, INT_MAX);
+        visited[src] = 0;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            auto [u, cost, stops] = q.front();
+            q.pop();
+
+            if (u == dst) {
+                ans = min(ans, cost);
+                continue;
+            }
+
+            if (stops > k) continue; // exceeded stop limit
+
+            for (auto &[v, w] : adj[u]) {
+                int newCost = cost + w;
+                // Only push if we found a cheaper way to reach v
+                if (newCost < visited[v]) {
+                    visited[v] = newCost;
+                    q.push({v, newCost, stops + 1});
+                }
+            }
+        }
+
+        return ans == INT_MAX ? -1 : ans;
     }
 };
