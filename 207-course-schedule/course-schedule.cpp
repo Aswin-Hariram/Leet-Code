3f34ae540b13ce9eb5879855222cf868dba21d41
Aswin Hariram <aswincseskct@gmail.com>
2026-05-03 12:59:46 +0530

    Time: 15 ms (10.22%) | Memory: 20.1 MB (32.40%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,44 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> create_adj(vector<vector<int>> &edges, int V) {
-        vector<vector<int>> adj(V);
-        for (auto &e : edges) {
-            int course = e[0];
-            int prereq = e[1];
-            adj[prereq].push_back(course);  // directed edge prereq -> course
-        }
-        return adj;
-    }
 
-    bool cycleUtil(vector<vector<int>> &adj, int node, vector<bool> &visited, vector<bool> &recStack) {
-        if (recStack[node]) return true;
-        if (visited[node]) return false;
 
-        visited[node] = true;
-        recStack[node] = true;
+    bool check(int node,vector<bool> &rec,vector<bool> &vis,vector<vector<int>> &adj){
 
-        for (int nei : adj[node]) {
-            if (cycleUtil(adj, nei, visited, recStack)) 
+        if(rec[node]) return true;
+        if(vis[node]) return false;
+
+
+        vis[node] = true;
+        rec[node] = true;
+
+
+        for(int nei:adj[node]){
+            if(check(nei,rec,vis,adj)){
                 return true;
+            }
         }
-
-        recStack[node] = false;
-        return false;
-    }
-
-    bool checkCycle(vector<vector<int>> &adj, int V) {
-        vector<bool> visited(V, false);
-        vector<bool> recStack(V, false);
-
-        for (int i = 0; i < V; i++) {
-            if (!visited[i] && cycleUtil(adj, i, visited, recStack))
-                return true;
-        }
+        rec[node] = false;
         return false;
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj = create_adj(prerequisites, numCourses);
-        return !checkCycle(adj, numCourses);
+        vector<vector<int>> adj(numCourses);
+
+
+        for(auto p: prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
+        vector<bool> vis(numCourses,false);
+        vector<bool> rec(numCourses,false);
+
+
+        for(int i = 0; i < numCourses; i++){
+            if(!vis[i]){
+                if(check(i, rec, vis, adj)){
+                    return false;
+                }
+            }
+        }
+
+
+        return true;
+
+
+
     }
 };
