9953734638288f33a11f9c60d10396c945ad18a5
Aswin Hariram <aswincseskct@gmail.com>
2026-05-11 22:46:58 +0530

    Time: 4 ms (67.64%) | Memory: 134.3 MB (83.37%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int size = (k*2)+1;
+        vector<int> res(nums.size(),-1);
+
+        
+
+        double sum = 0;
 
         int left = 0;
-        double sum = 0;
-        vector<int> res(nums.size(),-1);
         for(int right = 0; right < nums.size();right++ ){
 
             sum += nums[right];
 
             if(right - left + 1 == size){
                 double avg = sum/size;
-                res[left + k]=avg;
+                res[(left+right)/2]=avg;
 
                 sum -= nums[left];
                 left++;
