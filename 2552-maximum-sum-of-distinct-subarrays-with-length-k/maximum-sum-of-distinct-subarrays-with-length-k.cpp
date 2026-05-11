8af54aed6e01ddee5d5a5e125707056dfd421019
Aswin Hariram <aswincseskct@gmail.com>
2026-05-11 23:34:59 +0530

    Time: 103 ms (56.99%) | Memory: 95.2 MB (73.45%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        
-        unordered_map<int,int> freq;
+        unordered_map<int,int> mp; // num -> freq
 
-        long long sum = 0;
+
+        int left =0;
+
+        long long sum =0;
         long long ans = 0;
 
-        int left = 0;
-
-        for(int right = 0; right < nums.size(); right++) {
+        for(int right =0;right<nums.size();right++){
 
             sum += nums[right];
-            freq[nums[right]]++;
+            mp[nums[right]]++;
 
-            // Maintain window size k
-            if(right - left + 1 > k) {
+            if(right - left +1 ==k ){
 
-                freq[nums[left]]--;
-
-                if(freq[nums[left]] == 0) {
-                    freq.erase(nums[left]);
+                if(mp.size()==k){
+                    ans = max(ans,sum);
                 }
 
                 sum -= nums[left];
+                mp[nums[left]]--;
+                if(mp[nums[left]] == 0 ){
+                    mp.erase(nums[left]);
+                }
                 left++;
+
+
             }
 
-            // Valid window
-            if(right - left + 1 == k && freq.size() == k) {
-                ans = max(ans, sum);
-            }
+
         }
 
         return ans;
