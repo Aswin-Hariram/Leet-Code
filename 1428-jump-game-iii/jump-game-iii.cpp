6d1b766a74ae7a668dda79fc5b4d6d2d70a28154
Aswin Hariram <aswincseskct@gmail.com>
2026-05-17 20:08:33 +0530

    Time: 3 ms (45.57%) | Memory: 39 MB (40.81%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
 
-    bool solve(vector<int>& arr, int start,vector<bool> &vis,vector<bool> &rec,int n){
-        if(start<0 || start>= n) return false;
-        if(arr[start]==0) return true;
-        if(rec[start]) return false;
-        
-        rec[start] = true;
-        bool pos = solve(arr,start+arr[start],vis,rec,n);
+    bool solve(vector<int>& arr, int start, vector<bool> &vis, int n) {
+        // Out of bounds
+        if(start < 0 || start >= n) return false;
 
-        bool neg = solve(arr,start-arr[start],vis,rec,n);
-        rec[start] = false;
-        return pos||neg;
+        // Already visited
+        if(vis[start]) return false;
 
+        // Found zero
+        if(arr[start] == 0) return true;
+
+        vis[start] = true;
+
+        // Move forward or backward
+        return solve(arr, start + arr[start], vis, n) ||
+               solve(arr, start - arr[start], vis, n);
     }
+
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<bool> rec(n,false);
-        vector<bool> vis(n,false);
-        bool ans = solve(arr,start,vis,rec,n);
-        return ans;
+        vector<bool> vis(n, false);
+        return solve(arr, start, vis, n);
     }
 };
