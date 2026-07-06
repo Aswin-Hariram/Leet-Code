8c27cdd5c6a88711c4ef7136f1a5cdb0b7536ff3
Aswin Hariram <aswincseskct@gmail.com>
2026-07-06 20:37:35 +0530

    Time: 109 ms (32.79%) | Memory: 95.1 MB (93.36%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mp; // num -> freq
+        int n = nums.size();
 
+        unordered_map<int,int> mp;
+        int left = 0;
+        long long sum = 0;
 
-        int left =0;
-
-        long long sum =0;
         long long ans = 0;
+        for(int right=0;right<n;right++){
+           mp[nums[right]]++;
+            sum+=nums[right];
 
-        for(int right =0;right<nums.size();right++){
-
-            sum += nums[right];
-            mp[nums[right]]++;
-
-            if(right - left +1 ==k ){
-
+            if(right-left+1==k){
                 if(mp.size()==k){
                     ans = max(ans,sum);
                 }
-
-                sum -= nums[left];
+                sum -=nums[left];
                 mp[nums[left]]--;
-                if(mp[nums[left]] == 0 ){
+                if(mp[nums[left]]==0){
                     mp.erase(nums[left]);
+
                 }
                 left++;
 
-
             }
 
-
         }
-
         return ans;
+
     }
 };
