dd949d7b1a52f4d942945d79fb259ee73c36ffc5
Aswin Hariram <aswincseskct@gmail.com>
2026-07-21 23:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (57.98%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int n,m;
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color,int prev){
+    void solve(int row, int col, vector<vector<int>>& image, int color,
+               int orginalColor) {
 
-        if (sr < 0 || sr >= n || sc < 0 || sc >= m) return;
-        if (image[sr][sc] != prev) return;
-        image[sr][sc] = color;
-        dfs(image,sr+1,sc,color,prev);
-        dfs(image,sr-1,sc,color,prev);
-        dfs(image,sr,sc+1,color,prev);
-        dfs(image,sr,sc-1,color,prev);
+        int n = image.size();
+        int m = image[0].size();
 
+        if (row < 0 || row >= n || col < 0 || col >= m ||
+            image[row][col] != orginalColor) {
+            return;
+        }
+        image[row][col] = color;
+
+        solve(row + 1, col, image, color, orginalColor);
+        solve(row - 1, col, image, color, orginalColor);
+        solve(row, col + 1, image, color, orginalColor);
+        solve(row, col - 1, image, color, orginalColor);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        n = image.size();
-        m = image[0].size();  
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
 
-        if (image[sr][sc] == color) return image;      
-        dfs(image,sr,sc,color,image[sr][sc]);
+        if (image[sr][sc] == color) {
+            return image;
+        }
+        solve(sr, sc, image, color, image[sr][sc]);
+
         return image;
     }
 };
